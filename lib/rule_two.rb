bbb2cc022428e58ac7ab2a6a8218eddeb5eee52a
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-08-23 18:42:11 +0100

    Implement rule 2.

--- lib/rule_two.rb
+++ lib/rule_two.rb
@@ -1,7 +1,34 @@
+#Understands how to update the next generation of alive cells based on rule 2:
+#Any live cell with two or three live neighbours lives on to the next generation.
+
 class RuleTwo
 
   def self.evaluate_next_generation_cells(grid)
-    []
+    current_generation = grid.current_generation
+    current_generation.select { |cell| has_two_or_three_neighbours?(cell, current_generation) }
+  end
+
+  #private
+
+  def self.has_two_or_three_neighbours?(cell, current_generation)
+    cell_neighbours = neighbours(cell)
+    current_generation.select { |cell| cell_neighbours.include?(cell) }.count.between?(2,3)
+  end
+
+#consider extracting a Cell class that understands who its neighbours are
+#also refactor into a more elegant solution?
+  def self.neighbours(cell)
+    x = cell[0]
+    y = cell[1]
+    [[x + 1, y],
+     [x + 1, y - 1],
+     [x, y - 1],
+     [x - 1, y - 1],
+     [x - 1, y],
+     [x -1, y + 1],
+     [x, y + 1],
+     [x + 1, y + 1]]
   end
 
 end
+
--- spec/unit/rule_two_spec.rb
+++ spec/unit/rule_two_spec.rb
@@ -1,16 +1,34 @@
-#Understands how to update the next generation of alive cells based on rule 2:
-#Any live cell with two or three live neighbours lives on to the next generation.
-
-describe RuleTwo, '#evaluate_next_generation_cells' do
+describe RuleTwo, ':evaluate_next_generation_cells' do
 
   let(:mock_grid) { instance_double('Grid') }
   let(:rule_two)  { described_class }
 
-  context 'When none of the cells in the current generation fit the rule', focus: true do
+  context 'When none of the cells in the current generation fit the rule' do
     it 'returns an empty array, example 0' do
       allow(mock_grid).to receive(:current_generation) { [] }
       expect(rule_two.evaluate_next_generation_cells(mock_grid)).to eq([])
     end
+
+    it 'returns an empty array, example 1' do
+      allow(mock_grid).to receive(:current_generation) { [[0,0], [1,0] ] }
+      expect(rule_two.evaluate_next_generation_cells(mock_grid)).to eq([])
+    end
   end
 
+  context 'When some of the cells in the current generation fit the rule' do
+    it 'returns an array of cells that fit the rule, example 0' do
+      allow(mock_grid).to receive(:current_generation) { [[0, 0], [1, 1], [-1, -1]] }
+      expect(rule_two.evaluate_next_generation_cells(mock_grid)).to eq([[0,0]])
+    end
+
+    it 'returns an array of cells that fit the rule, example 1' do
+      allow(mock_grid).to receive(:current_generation) { [[0, 0], [1, 1], [-1, -1], [0, -1]] }
+      expect(rule_two.evaluate_next_generation_cells(mock_grid)).to eq([[0,0],[-1,-1],[0,-1]])
+    end
+
+    it 'returns an array of cells that fit the rule, example 2' do
+      allow(mock_grid).to receive(:current_generation) { [[0, 0], [-1, 1], [1, -1], [0, -2]] }
+      expect(rule_two.evaluate_next_generation_cells(mock_grid)).to eq([[0,0],[1,-1]])
+    end
+  end
 end
